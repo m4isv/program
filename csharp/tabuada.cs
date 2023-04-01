585c270d07d4998673ce3eff41e35c935f7cd3c0
M4isv <53246704+m4isv@users.noreply.github.com>
2023-03-31 22:26:28 -0300

    Add files via upload
    tabuada

---
+++ csharp/tabuada.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace APP
+{
+    class Program
+    {
+      static void Main(string[] args)
+      {
+       
+        for(int numero = 0; numero <10; numero++){
+            Console.WriteLine($"{numero} X {numero} = {numero * numero}");
+        }
+      
+
+      }
+    }
+}
